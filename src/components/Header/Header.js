9fdada15db17c51f70c106b09600004676eca529
Rejaur9131 <raju_seu@yahoo.com>
2022-03-26 15:33:37 +0600

    project initialized with responsiveness

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="text-center bg-info px-2">
+      <h1 className="text-danger px-2">Welcome to Leather Bag's World!!!</h1>
+      <h3>Choose Any 4 Items</h3>
+    </div>
+  );
+};
+
+export default Header;
